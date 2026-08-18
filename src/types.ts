830a17079af9c4347ed7511b1dd3662661032dbb
kfkal1809 <kfkal1809@gmail.com>
2026-08-18 17:55:34 +0900

    게임 구현: src/types.ts

---
+++ src/types.ts
@@ -0,0 +1,47 @@
+export type CharacterKey = 'haenam' | 'haenyeo'
+
+export type Category =
+  | 'hair'
+  | 'top'
+  | 'bottom'
+  | 'outfit'
+  | 'shoes'
+  | 'hat'
+  | 'accessory'
+  | 'background'
+
+export interface DressItem {
+  id: string
+  name: string
+  category: Exclude<Category, 'background'>
+  gender: CharacterKey
+  color: string
+  accent: string
+  icon: string
+}
+
+export interface BackgroundItem {
+  id: string
+  name: string
+  category: 'background'
+  color: string
+  accent: string
+  icon: string
+}
+
+export interface CharacterState {
+  nickname: string
+  hair: string
+  top: string
+  bottom: string
+  outfit: string | null
+  shoes: string
+  hat: string | null
+  accessory: string | null
+}
+
+export interface GameState {
+  haenam: CharacterState
+  haenyeo: CharacterState
+  background: string
+}
